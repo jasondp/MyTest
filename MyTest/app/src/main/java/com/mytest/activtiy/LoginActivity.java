78ef3454a6e269fea1478ddb5253f87de7eb76f3
xiongcai <xiongcai@med-corp.net>
2016-11-28 02:16:00 +0800

    [add new packet]

--- MyTest/app/src/main/java/com/mytest/activtiy/LoginActivity.java
+++ MyTest/app/src/main/java/com/mytest/activtiy/LoginActivity.java
@@ -5,8 +5,12 @@ import android.support.annotation.Nullable;
 
 import com.mytest.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Created by Jason on 2016/11/24.
+ *
  */
 
 public class LoginActivity extends BaseActivity {
@@ -15,5 +19,11 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_login_layout_activity);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.login_user_to_register_button)
+    public void openRegisterActivity() {
+        startActivity(RegisterActivity.class);
     }
 }
